40b33233dadd16d8b062eb75beff270267ab1258
milarancic4 <mila.rancic4@gmail.com>
2022-09-16 23:03:55 +0200

    Two models added

--- models/model.py
+++ models/model.py
@@ -13,6 +13,22 @@ from tensorflow.keras.layers import Concatenate
 from tensorflow.keras.models import Model
 import tensorflow as tf
 
+def cnn1(height, width, depth, classes):
+    inputShape = (height, width, depth)
+    model = Sequential([
+        Conv2D(filters=64, kernel_size=(5, 5), input_shape=inputShape, activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+        Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+
+        Flatten(),
+        Dense(64, activation='relu'),
+        Dense(classes, activation='softmax')
+    ])
+    return model
+
 def cnn2(height, width, depth, classes):
     inputShape = (height, width, depth)
     model = Sequential([
@@ -33,6 +49,47 @@ def cnn3(height, width, depth, classes):
         MaxPooling2D(pool_size=(2, 2)),
 
         Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+
+        Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+
+        Flatten(),
+        Dense(128, activation='relu'),
+
+        Dropout(0.25),
+        Dense(classes, activation='softmax')
+    ])
+    return model
+def cnn4(height, width, depth, classes):
+    inputShape = (height, width, depth)
+    model = Sequential([
+        Conv2D(filters=64, kernel_size=(5, 5), input_shape=inputShape, activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+        Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+
+        Flatten(),
+        Dense(64, activation='relu'),
+        Dropout(0.25),
+        Dense(classes, activation='softmax')
+    ])
+    return model
+def cnn5(height, width, depth, classes):
+    inputShape = (height, width, depth)
+    model = Sequential([
+        Conv2D(filters=32, kernel_size=(5, 5), input_shape=inputShape, activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+
+        Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+
+        Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'),
         MaxPooling2D(pool_size=(2, 2)),
         Dropout(0.25),
 
